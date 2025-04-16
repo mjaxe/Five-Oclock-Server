2d4e47d41f2a5e778c97563d59748a51fac41a00
Michael Akstin <makstin33@gmail.com>
2025-04-16 17:54:56 -0400

    Adding in changes to try and allow local host to talk to each other

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const cors = require('cors');
 const zoneRoutes = require('./controllers/zoneController');
 
 app.use(express.json());
@@ -9,3 +10,7 @@ app.use('/zones', zoneRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use(cors({
+  origin: 'http://localhost:4200'  // <-- Allow frontend
+}));
