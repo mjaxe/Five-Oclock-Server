19b83ecb2567f53a5b82f3b975ba0303b6f1f77f
Michael Akstin <makstin33@gmail.com>
2025-09-15 15:12:27 -0400

    cleaning up code

--- api/index.js
+++ api/index.js
@@ -1,20 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const PORT = 3000;
-// const cors = require('cors');
-// const zoneRoutes = require('./controllers/zoneController');
-
-// app.use(cors({
-//   origin: ['http://localhost:4200', 'https://five-oclock-client.vercel.app','https://five-oclock-client-git-main-mjaxes-projects.vercel.app'], // Add your deployed frontend here if needed
-// }));
-
-// app.use(express.json());
-// app.use('/zones', zoneRoutes);
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
 const express = require('express');
 const cors = require('cors');
 const zoneRoutes = require('../controllers/zoneController.js');
